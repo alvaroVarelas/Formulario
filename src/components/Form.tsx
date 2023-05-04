d0c9b2e3ccc7e94e81eab0c28a9cef2cae8899b6
Alvaro Varelas <alvaro.varelas@udea.edu.co>
2023-05-03 21:21:46 -0500

    hacer el handle y el onSubmit con los datos a mostrar y arreglar lo del client

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,20 @@
+'use client'
 import { Input } from '@/components/Input'
 import { Button } from '@/components/Button'
+import React from 'react'
 
 export const Form = () => {
+     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      const form = e.currentTarget
+      const formData = new FormData(form)
+      const { id, nombre, decano } = Object.fromEntries(formData.entries())
+      console.log({ id, nombre, decano })// luego componerlo con el backend
+
+     } 
+
     return (
-        <form className="space-y-8 border border-white/10 p-8 rounded">
+        <form onSubmit={handleSubmit} className="space-y-8 border border-white/10 p-8 rounded">
         <Input 
           id="id"
           name="id"
